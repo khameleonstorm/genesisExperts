22e32d631719720d26084916cd0482aaf652f483
khameleonstorm <khameleonstorm@gmail.com>
2023-04-25 13:21:58 +0100

    address

--- src/components/funding/Funding.js
+++ src/components/funding/Funding.js
@@ -22,16 +22,16 @@ let coin = {
     title: "Bitcoin",
     image: qr1,
     network: "BTC",
-    address: "bc1qxf9rtsjg6dpdapqex7k76dp53ufcvhwcckt8s3",
-    link: "https://link.trustwallet.com/send?asset=c0&address=bc1qxf9rtsjg6dpdapqex7k76dp53ufcvhwcckt8s3",
+    address: "bc1q3d0h3s2zspaw32ex75av6w9n20vxkn36z23mzn",
+    link: "https://link.trustwallet.com/send?coin=0&address=bc1q3d0h3s2zspaw32ex75av6w9n20vxkn36z23mzn",
   },
   ethereum: {
     id: 2,
     title: "Ethereum",
     image: qr2,
     network: "Erc 20",
-    address: "0x35493fedFA79Eb2831E1F8F05a70C1C0144c28cD",
-    link: "https://link.trustwallet.com/send?address=0x35493fedFA79Eb2831E1F8F05a70C1C0144c28cD&asset=c60",
+    address: "0xA2035847428DD26Cc312c98Fb105930395e7f0cc",
+    link: "https://link.trustwallet.com/send?coin=60&address=0xA2035847428DD26Cc312c98Fb105930395e7f0cc",
   }};
 
 
